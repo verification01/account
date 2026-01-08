e0f3199a0effe0d3f4d28b77ce7e90661af11c26
verification01 <verifyproject11@gmail.com>
2026-01-08 11:09:37 +0600

    Create style.js

---
+++ style.js
@@ -0,0 +1,15 @@
+function access(){
+const inpt = document.getElementById("holo-input").value;
+if (inpt === "saad"){
+window.location.href = "saadac.html";
+}
+else if(inpt === "santo"){
+window.location.href = "santoac.html";
+}
+else if(inpt === "*1015@14#"){
+window.location.href = "sabitac.html";
+}
+else{
+alert("Access Denied! User not found.")
+}
+}
